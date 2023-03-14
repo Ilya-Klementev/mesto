d5900bc58cc2a563969bc496e1a94ee5590878f4
Ilya Klementev <klementevvv@yandex.ru>
2023-03-14 22:15:56 +0100

    первый вариант работы, после проверки ревьювера

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
-const popupElement = document.querySelector(".popup");
-const popupCloseButtonElement = popupElement.querySelector(".popup__close"); 
-const popupOpenButtonElement = document.querySelector(".profile__edit-button"); 
-const pageNameElement = document.getElementById('name');
-const popupNameElement = document.getElementById('popupName');
-const pageAboutElement = document.getElementById('about');
-const popupAboutElement = document.getElementById('popupAbout');
-const popupSubmitButtonElement = popupElement.querySelector(".popup__submit");
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close'); 
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const pageNameElement = document.querySelector('.profile__name');
+const popupNameElement = popupElement.querySelector('.popup__input_element_name');
+const pageAboutElement = document.querySelector('.profile__about');
+const popupAboutElement = popupElement.querySelector('.popup__input_element_about');
+const popupFormElement = popupElement.querySelector('.popup__form');
+const popupSubmitButtonElement = popupFormElement.querySelector('.popup__submit');
+const pageHeartElements = document.querySelectorAll('.place__heart');
+
 
 const PopupElementOpened = function () {
   popupElement.classList.add('popup_opened');
@@ -17,12 +20,20 @@ const PopupElementClosed = function () {
   popupElement.classList.remove('popup_opened');
 }
 
-const PopupElementSubmit = function () {
-  popupElement.classList.remove('popup_opened');
+const PopupElementSubmit = function (evt) {
+  PopupElementClosed();
   pageNameElement.textContent = popupNameElement.value;
   pageAboutElement.textContent = popupAboutElement.value;
+  evt.preventDefault();
+}
+
+for (let i = 0; i < pageHeartElements.length; i++) {
+  const pageHeartElement = pageHeartElements[i];
+  pageHeartElement.addEventListener('click', function(){
+    pageHeartElement.classList.toggle('place__heart_liked');
+  });
 }
 
 popupOpenButtonElement.addEventListener('click', PopupElementOpened);
 popupCloseButtonElement.addEventListener('click', PopupElementClosed);
-popupSubmitButtonElement.addEventListener('click', PopupElementSubmit);
+popupFormElement.addEventListener('submit', PopupElementSubmit);
